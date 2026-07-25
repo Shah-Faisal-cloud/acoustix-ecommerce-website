5509b8ab842f21a70ced8ff253c55bb557409621
Shah-Faisal-cloud <fjcreater99@gmail.com>
2026-07-25 11:36:44 +0500

    added categories section

--- src/js/pages/homePage.js
+++ src/js/pages/homePage.js
@@ -10,6 +10,7 @@ function initHomePage() {
       <main>
         ${heroSection()}
         ${categories()}
+        ${bannersSection()}
       </main>
     `;
 
@@ -34,6 +35,42 @@ function heroSection() {
     </div>
   </section>
   `;
+}
+
+function bannersSection() {
+  return /* html */ `
+    <section class="banners-wrapper">
+    <div class="zx9-banner">
+      <div class="zx9-banner__media">
+        <img src="/image-speaker-zx9.png" alt="ZX9 Speaker" class="zx9-banner__img" />
+      </div>
+    
+      <div class="zx9-banner__content">
+        <h2 class="zx9-banner__title">ZX9<br />Speaker</h2>
+        <p class="zx9-banner__description">
+          Upgrade to premium speakers that are phenomenally built to deliver truly remarkable sound.
+        </p>
+        ${button({ color: 'black', isOutline: false, url: '/pages/speakers.html?product=zx9' })}
+      </div>
+    </div>
+
+    <div class="zx7-banner">
+      <div class="zx7-banner__content">
+        <h2 class="zx7-banner__title">ZX7 Speaker</h2>
+        ${button({ color: 'black', isOutline: true, url: '/pages/speakers.html?product=zx7' })}
+      </div>
+    </div>
+
+    <div class="yx1-banner">
+      <div class="yx1-banner__media"></div>
+    
+      <div class="yx1-banner__content">
+        <h2 class="yx1-banner__title">YX1 Earphones</h2>
+        ${button({ color: 'black', isOutline: true, url: '/pages/earphones.html?product=yx1' })}
+      </div>
+    </div>
+    </section>
+  `
 }
 
 window.addEventListener("DOMContentLoaded", initHomePage);
