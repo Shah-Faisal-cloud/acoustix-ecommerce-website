a881d39b3cfda88618490a47c0c6893028342d83
Shah-Faisal-cloud <fjcreater99@gmail.com>
2026-07-28 18:01:30 +0500

    updated

--- src/js/components/aboutUs.js
+++ src/js/components/aboutUs.js
@@ -17,7 +17,7 @@ export default function AboutUs() {
         Located at the heart of New York City, Acoustix is the premier store for high end headphones,
         earphones, speakers, and audio accessories. We have a large showroom and luxury demonstration
         rooms available for you to browse and experience a wide range of our products. Stop by our store
-        to meet some of the fantastic people who make Audiophile the best place to buy your portable audio equipment.
+        to meet some of the fantastic people who make Acoustix the best place to buy your portable audio equipment.
       </p>
     </div>
   </section>
